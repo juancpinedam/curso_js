787bf11a019c4e386e9fe2f20eaae18595e0025f
jpineda <jpineda@aceitesa.com>
2021-11-24 08:26:37 -0500

    elementos

--- funcionesCadena.js
+++ funcionesCadena.js
@@ -1,6 +1,6 @@
 let nombre ="juan";
 let apellido="pineda";
-let frase ="no nombre es juan pinedo";
+let frase =" no nombre es juan pinedo ";
 
 let nombreCompleto = nombre.concat(apellido);
 
@@ -12,6 +12,8 @@ let arrayFrase = frase.split(" ");
 
 let subFrase = frase.substring(3,14);
 
+let fraseSinESpacio = frase.trim();
+
 document.write(respuesta);
 document.write("<hr>");
 document.write(indexNombre);
@@ -21,3 +23,15 @@ document.write("<hr>");
 document.write("el tercer token: "+arrayFrase[2]);
 document.write("<hr>");
 document.write("el sub string: "+subFrase);
+document.write("<hr>");
+document.write("sin espaciosss: "+frase);
+document.write("<hr>");
+document.write("sin espaciosss: "+fraseSinESpacio);
+document.write("<hr>");
+document.write("lengh con espaciosss: "+frase.length);
+document.write("<hr>");
+document.write("lengh sin espaciosss: "+fraseSinESpacio.length);
+
+const campo1 = document.getElementById("campo1");
+document.write(campo1);
+campo1.setAttribute("contentEditable","false");
